10b92abcba15cb02fe40e8f11c05d0ffa366cce5
Desislava Stoyanova <dstoyanova@desislavas-macbook-air.local>
2015-09-20 20:28:18 +0200

    Initial Commit

---
+++ LACPP-Assignment-1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LACPP-Assignment-1
+//
+//  Created by Desislava Stoyanova on 9/20/15.
+//  Copyright © 2015 Desislava Stoyanova. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
