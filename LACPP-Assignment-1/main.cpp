26ff0f41236b5ce2065ecde7c6faa5d7027d170a
Desislava Stoyanova <dstoyanova@desislavas-macbook-air.local>
2015-09-25 12:08:35 +0200

    Final version of the main function.

--- LACPP-Assignment-1/main.cpp
+++ LACPP-Assignment-1/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+#include <cmath>
 #include <thread>
 #include <mutex>
 #include <vector>
 
 using namespace std;
 
+// Holds the final result
 float sum = 0.0;
 mutex m;
 
@@ -12,10 +14,12 @@ float f(float x) {
     return 4.0 / (1.0 + x*x);
 }
 
-void area(float a,float b,float c) {
-    m.lock();
-    sum += (a + b) * c / 2.0;
-    m.unlock();
+void func(float start,float end,float height) {
+    for (float i = start; i < end; i += height) {
+        m.lock();
+        sum += (f(i) + f(i + height)) * height / 2.0;
+        m.unlock();
+    }
 }
 
 int main(int argc, const char * argv[]) {
@@ -26,10 +30,23 @@ int main(int argc, const char * argv[]) {
     
     vector<thread> th;
     
+    // Calculates the number of trapezes per thread
+    float temp = floor((float)trapezes / (float)threads);
+    
     float h = 1.0 / (float)trapezes;
     
-    for (float i = 0.0; i < 1.0; i += h) {
-        th.push_back(thread(area,f(i),f(i+h),h));
+    int br = 0;
+    
+    for (float i = 0.0; i < 1.0; i += temp * h) {
+        // Our final thread will calculate the rest of the trapezes we have
+        if (br == threads - 1) {
+            th.push_back(thread(func,i,1.0,h));
+            br++;
+        }
+        else {
+            th.push_back(thread(func,i,temp*h,h));
+            br++;
+        }
     }
     
     for (auto &t : th) {
